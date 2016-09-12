61ca16ccb20971a6a62e398cde3f9a03e3c40c36
Nick Pisciotta <npisciotta@gmail.com>
2016-09-11 18:41:38 -0600

    Change the users table

--- db/migrate/20160912003905_create_users.rb
+++ db/migrate/20160912003905_create_users.rb
@@ -1,11 +1,11 @@
-class CreateUser < ActiveRecord::Migration[5.0]
+class CreateUsers < ActiveRecord::Migration[5.0]
   def change
-    create_table :user do |t|
+    create_table :users do |t|
       t.string :email
       t.string :password_digest
       t.string :password_confirmation
 
-      t.timestamps null:false 
+      t.timestamps null:false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912003302) do
+ActiveRecord::Schema.define(version: 20160912003905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "user", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
     t.string   "password_confirmation"
