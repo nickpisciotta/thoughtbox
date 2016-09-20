cc28d88eca22f6267875d042c106af40eb4b6021
Nick Pisciotta <npisciotta@gmail.com>
2016-09-20 10:24:57 -0600

    Fix login issue and add sad/happy path tests for login

--- spec/features/user_can_login_logout_spec.rb
+++ spec/features/user_can_login_logout_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "User cannot login" do
-  scenario "with invalid email and password" do
+RSpec.feature "User login" do
+  scenario "cannot login with invalid email and password" do
     visit root_path
 
     within('.navbar') do
@@ -19,5 +19,29 @@ RSpec.feature "User cannot login" do
       fill_in "session[password]", with: "password"
       click_on "Log In"
     end
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content("Invalid Login")
+  end
+
+  scenario "can login with valid email and password" do
+    visit root_path
+
+    click_on "Sign Up"
+
+    sign_up_user
+
+    click_on "Log Out"
+
+    expect(current_path).to eq(login_path)
+
+    within(".login-form") do
+      fill_in "session[email]", with: 'fake@email.com'
+      fill_in "session[password]", with: 'password'
+      click_on "Log In"
+    end
+
+    expect(current_path).to eq(links_path)
+    expect(page).to have_link("Log Out")
   end
 end
